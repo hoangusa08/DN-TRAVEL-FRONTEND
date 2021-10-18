e2ceef01c4b913e32741833ec2df5361294e2524
văn hoàng đoàn <you@example.com>
2021-10-19 00:04:45 +0700

    create project

---
+++ src/components/TopBarMenu/TopBarMenu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./TopBarMenu.scss";
+
+export default function TopBarMenu() {
+  return (
+    <div className="TopBarMenu">
+      <div className="row custom-container">
+        <div className="col">
+          <div className="">liên hệ : 0339905697</div>
+        </div>
+        <div className="col">
+          <div className="row">
+            <a>asxac</a>
+            <a></a>
+            <a></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TopBarMenu from "../../components/TopBarMenu/TopBarMenu";
+
+export default function Home() {
+  return (
+    <div>
+      <TopBarMenu />
+    </div>
+  );
+}
