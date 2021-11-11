a293a99eba5bc512e66d3299f55e9ab7e58bad18
hoangusa08 <hoangusa08@gmail.com>
2021-11-11 09:37:17 +0700

    Update the pagination bar

--- src/pages/tour/tour.js
+++ src/pages/tour/tour.js
@@ -1,35 +1,13 @@
-import React, { useEffect, useState } from "react";
-import ReactPaginate from "react-paginate";
+
 import CardTourDetail from "../../components/CardTourDetail/CardTourDetail";
+import Pagination from "../../components/Pagination/Pagination";
 import SlideBarLeft from "../../components/SlideBarLeft/SlideBarLeft";
 import MainLayout from "../../layout/MainLayout";
 import Function from "./Function/Function";
 import "./tour.scss";
 
 export default function Tour() {
-  let itemsPerPage = 1;
-  const [currentItems, setCurrentItems] = useState(null);
-  const [pageCount, setPageCount] = useState(0);
-  // Here we use item offsets; we could also use page offsets
-  // following the API or data you're working with.
-  const [itemOffset, setItemOffset] = useState(0);
-
-  useEffect(() => {
-    // Fetch items from another resources.
-    const endOffset = itemOffset + itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-    setCurrentItems(items.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(items.length / itemsPerPage));
-  }, [itemOffset, itemsPerPage]);
-
-  // Invoke when user click to request another page.
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
-    setItemOffset(newOffset);
-  };
+ 
 
   return (
     <MainLayout>
@@ -44,33 +22,10 @@ export default function Tour() {
           </div>
         </div>
         <div>
-          <Items currentItems={currentItems} />
-          <ReactPaginate
-            breakLabel="..."
-            nextLabel="next >"
-            onPageChange={handlePageClick}
-            pageRangeDisplayed={5}
-            pageCount={pageCount}
-            previousLabel="< previous"
-            renderOnZeroPageCount={null}
-          />
+         <Pagination/>
         </div>
       </div>
     </MainLayout>
   );
 }
 
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
-function Items({ currentItems }) {
-  return (
-    <>
-      {currentItems &&
-        currentItems.map((item) => (
-          <div>
-            <h3>Item #{item}</h3>
-          </div>
-        ))}
-    </>
-  );
-}
